86523cad42e3661df76a3f16ad2629c35d25d85c
2081902M <munro600@gmail.com>
2015-01-15 19:38:36 +0000

    problem with link

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. This tutorial has been put together by Nicole Munro, 2081902 <a href=\"/rango/\">Index</a>")
+    return render(request, 'rango/about.html')
