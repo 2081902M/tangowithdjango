0620a8721aab4fc33007e8436d4082ffe62458a8
2081902M <munro600@gmail.com>
2015-01-25 09:35:41 +0000

    working on exercise

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,55 +12,65 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=12)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=25)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=3)
 
     django_cat = add_cat("Django", 64, 32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=32)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=13)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=23)
 
     frame_cat = add_cat("Other Frameworks", 32, 16)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=16)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=31)
 
     name_cat = add_cat('Nicole Munro',0,0)
 
     add_page(cat=name_cat,
        title="GitHub",
-       url="https://github.com/2081902M/tangowithdjango")
+       url="https://github.com/2081902M/tangowithdjango",
+       views=9)
 
     add_page(cat=name_cat,
         title="pythonanywhere",
-        url="https://www.pythonanywhere.com/user/2081902M/files/home/2081902M")
+        url="https://www.pythonanywhere.com/user/2081902M/files/home/2081902M",
+        views=2)
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print "- {0} - {1}".format(str(c), str(p))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
