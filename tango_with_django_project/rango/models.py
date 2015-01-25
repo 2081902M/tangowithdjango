ebd910f0a1a8ee9681c83b97be21c14f77df3289
2081902M <munro600@gmail.com>
2015-01-25 14:42:47 +0000

    Chapter 7 - completed, unused code removed

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -21,9 +21,5 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
-    def save(self, *args, **kwargs):
-                self.slug = slugify(self.title)
-                super(Page, self).save(*args, **kwargs)
-
     def __unicode__(self):
         return self.title
