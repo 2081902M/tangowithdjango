a17d53a79ffbcc987758ab08b577103a426a67b7
2081902M <munro600@gmail.com>
2015-01-26 19:18:27 +0000

    working on exercises, tutorial completed

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -7,9 +7,9 @@ class CategoryForm(forms.ModelForm):
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-class Meta:
-    model = Category
-    fields = ('name',)
+    class Meta:
+        model = Category
+        fields = ('name',)
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
