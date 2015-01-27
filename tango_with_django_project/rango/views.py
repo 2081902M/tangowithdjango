c205741b7a20ce22e9a4c5ae80915937d69e78da
unknown <2081902M@BO706-13.ad.dcs.gla.ac.uk>
2015-01-27 10:48:10 +0000

    Chapter 9 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,6 +19,7 @@ def category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
+        context_dict['category_name_slug'] = category.slug
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
